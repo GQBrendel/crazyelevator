b58bec6ac2d9be22e9c6b68483fc7bf09b83427a
DemetrioBianchi <demetriobianchi@hotmail.com>
2019-09-30 03:28:38 -0300

    Revert "System of Light in Elevator"
    This reverts commit 4939099350b974da2a5861d03dce24a909f02a3f.

--- Assets/_Project/Scripts/Elevator/FloorMarkerCollider.cs
+++ Assets/_Project/Scripts/Elevator/FloorMarkerCollider.cs
@@ -6,27 +6,9 @@ public class FloorMarkerCollider : MonoBehaviour
 {
     private ElevatorController _elevatorController;
 
-    //system Light
-    [SerializeField] private Light[] lights;
-    private bool isClicked;
-
     private void Start()
     {
         _elevatorController = GetComponentInParent<ElevatorController>();
-    }
-
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            lights[0].enabled = false;
-            lights[1].enabled = false;
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            lights[0].enabled = true;
-            lights[1].enabled = true;
-        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -35,26 +17,6 @@ public class FloorMarkerCollider : MonoBehaviour
         if (floorMarker)
         {
             _elevatorController.FloorChanged(floorMarker);
-        }
-
-        if (other.gameObject.name == "Floor0")
-        {
-            lights[0].color = Color.green;
-        }else if (other.gameObject.name == "Floor1")
-        {
-            lights[0].color = Color.magenta;
-        }
-        else if (other.gameObject.name == "Floor2")
-        {
-            lights[0].color = Color.yellow;
-        }
-        else if (other.gameObject.name == "Floor3")
-        {
-            lights[0].color = Color.red;
-        }
-        else if (other.gameObject.name == "Floor4")
-        {
-            lights[0].color = Color.blue;
         }
     }
 }
