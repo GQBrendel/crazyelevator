79f259a73a1672c8659fbd4d9bb003b9ba2cf683
Guilherme Brendel <guilhermebrendel@gmail.com>
2019-10-16 00:34:49 -0300

    Includes new main menu.

---
+++ Assets/NewMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewMenu : MonoBehaviour
+{
+    [SerializeField] private Button _playButton;
+    [SerializeField] private Button _creditsButton;
+    [SerializeField] private Button _backButton;
+    [SerializeField] private GameObject _creditsPanel;
+
+    void Start()
+    {
+        _playButton.onClick.AddListener(() => { SceneManager.LoadScene(1); });
+        _creditsButton.onClick.AddListener(() => { _creditsPanel.SetActive(true); });
+        _backButton.onClick.AddListener(() => { _creditsPanel.SetActive(false); });
+    }
+}
